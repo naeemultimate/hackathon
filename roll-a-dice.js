c3755713d6ea77a06c87f125807c95c37847d327
Syed Naeem Shah <naeemultimate@gmail.com>
2025-01-17 11:34:48 +0000

    add loop function

--- roll-a-dice.js
+++ roll-a-dice.js
@@ -11,35 +11,26 @@ function startGame(){
     
     // Include sub functions e.g. prompt, executing random roll etc
     
-    let answer = prompt('Would you like to roll a dice? (yes/no):').toLowerCase();
+    while (true) {
+        let answer = prompt('Would you like to roll a dice? (yes/no):').toLowerCase();
 
-    if (answer === 'yes') {
-        let rollResult = rollTheDice();
-        console.log(`You rolled a ${rollResult}`)
-    } else if (answer === 'no') {
+        if (answer === 'yes') {
+            let rollResult = rollTheDice();
+            console.log(`You rolled a ${rollResult}`)
+        } else if (answer === 'no') {
         console.log("Thanks for playing!");
-    } else {
+        break;
+        } else {
         console.log("Please type yes or no");
+        
+        }
+
     }
-
+   
 }
-
-
 // function winner() {
 
 // }
 
-
 startGame() // <---- Starts game from here
-
-
-
-
-
-
-
-
-
-
-
 
